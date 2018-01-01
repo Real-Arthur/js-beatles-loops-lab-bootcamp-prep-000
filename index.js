307bed3ce1e0e68bc7a2e399f6b0e0f862b86213
Arthur Carson <carson.bob.arthur@gmail.com>
2018-01-01 12:00:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(x, y) {
+  var z = [];
+  for (let i = 0; i < 4; i++) {
+    z.push(`${x[i]} plays ${y[i]}`);
+  }
+  return z;
+  }
+  
+  function johnLennonFacts(x) {
+    var i = 0;
+    while (i < x.length) {
+      x.push([i] + "!!!");
+      i++;
+    }
+  }
